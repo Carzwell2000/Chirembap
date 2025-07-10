809920680f7bbfa6acdeeba4ece663e503b1f4ae
carzwell chimanyiwa <carzwellc@gmail.com>
2025-07-10 15:40:02 +0200

    first commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import Sidebar from '../Components/Sidebar'
-import Navbar from '../Components/Navbar'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react';
+import Sidebar from '../Components/Sidebar';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router-dom';
 
 function Dashboard() {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
     return (
-        <div>
-            <div className="flex">
+        <div className="flex flex-col md:flex-row min-h-screen">
+            {/* Sidebar */}
+            <div className={`${sidebarOpen ? 'block' : 'hidden'} md:block md:w-64`}>
                 <Sidebar />
             </div>
-            <div className=" flex-1 ml-64 p-8 h-screen">
-                <Navbar />
-                <Outlet />
-            </div>
 
+            {/* Main Content Area */}
+            <div className="flex-1 bg-gray-50">
+                {/* Top Navbar */}
+                <div className="sticky top-0 z-20">
+                    <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+                </div>
+
+                {/* Page Content */}
+                <div className="p-4">
+                    <Outlet />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Dashboard
+export default Dashboard;
+
