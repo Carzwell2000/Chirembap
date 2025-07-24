6e1a064a0c24545595ddbfedf6a17cf336efe222
carzwell chimanyiwa <carzwellc@gmail.com>
2025-07-24 11:33:50 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { UserProvider } from './Context/UserContext';
+import { UserProvider } from './Context/Usercontext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
