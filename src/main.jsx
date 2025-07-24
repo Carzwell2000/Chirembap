27d766c8d26a2309931206c59aee2019fb29cb85
carzwell chimanyiwa <carzwellc@gmail.com>
2025-07-24 13:47:51 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { UserProvider } from './Contexts/UserContexts.jsx';
+import { UserProvider } from './Contexts/UserContext.js';
+
 import './firebase/firebaseConfig';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
